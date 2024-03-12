3b5920f2b0c9483b837fd2fb00a931b69e8e910f
Ishita-Samadhiya <i.samadhiya@my.metroed.net>
2024-03-12 10:29:58 -0700

    1st

---
+++ Match/Match/MatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchApp.swift
+//  Match
+//
+//  Created by StudentAM on 3/12/24.
+//
+
+import SwiftUI
+
+@main
+struct MatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
