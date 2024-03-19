0f46c2a75e48a832b5c352f1b0c8479bf83edc5e
Ishita-Samadhiya <i.samadhiya@my.metroed.net>
2024-03-19 09:15:20 -0700

    start pookie

--- Match/Match/ContentView.swift
+++ Match/Match/ContentView.swift
@@ -9,14 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Image("start")
+                .resizable()
+                .scaledToFill()
+                .edgesIgnoringSafeArea(.all)
+            VStack{
+                Spacer()
+                Text("Fruit")
+                    .font(.custom("Silkscreen-Bold", size: 46))
+                Text("Match")
+                    .font(.custom("Silkscreen-Bold", size: 46))
+                Spacer()
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                    Text("Start")
+                        .font(.custom("Silkscreen-Bold", size: 46))
+                        .foregroundStyle(.black)
+                        .background(Color(UIColor(red:1, green:0.5 ,blue:0.5 , alpha:1.00)))
+                        .cornerRadius(10)
+                        .padding()
+                })
+                Spacer()
+            }
         }
-        .padding()
     }
+        
 }
 
 #Preview {
